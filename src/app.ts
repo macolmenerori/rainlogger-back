ed6f0ca4c16f7e1a6daf66f7f4211b0d9c9d4443
macolmenerori <miguelangelcolmenero@gmail.com>
2026-02-04 10:48:35 +0100

    feat: Added main structure and server setup

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Request, Response } from 'express';
+import rainloggerRouter from './routes/rainloggerRouter';
+
+const app = express();
+
+// Healthcheck
+app.get('/healthcheck', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Server is running'
+  });
+});
+
+app.use('api/v1/rainlogger', rainloggerRouter);
+
+// Middleware for handling unhandled routes
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`
+  });
+});
+
+export default app;
