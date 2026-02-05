8c0c147aa82b6939067e775542211b489d995425
macolmenerori <miguelangelcolmenero@gmail.com>
2026-02-05 08:57:48 +0100

    feat: Get Rainlog by filters

--- src/routes/rainloggerRouter.ts
+++ src/routes/rainloggerRouter.ts
@@ -1,17 +1,35 @@
 import express from 'express';
 
 import { protect } from '../controllers/authControllers';
-import { addRainlogToDatabase, getRainlogById } from '../controllers/rainlogControllers';
+import {
+  addRainlogToDatabase,
+  getRainlogById,
+  getRainlogFilters
+} from '../controllers/rainlogControllers';
 import { methodNotAllowed } from '../utils/methodNotAllowed';
-import { addRainlogValidation, getRainlogByIdValidation } from '../validations/rainlog.validations';
+import {
+  addRainlogValidation,
+  getRainlogByIdValidation,
+  getRainlogFiltersValidation
+} from '../validations/rainlog.validations';
 
 const router = express.Router();
 
 router
-
   .route('/rainlog')
   .post(protect, addRainlogValidation, addRainlogToDatabase)
   .get(protect, getRainlogByIdValidation, getRainlogById)
   .all(methodNotAllowed(['POST', 'GET']));
+
+router
+  .route('/rainlog')
+  .post(protect, addRainlogValidation, addRainlogToDatabase)
+  .get(protect, getRainlogByIdValidation, getRainlogById)
+  .all(methodNotAllowed(['POST', 'GET']));
+
+router
+  .route('/rainlog/filters')
+  .get(protect, getRainlogFiltersValidation, getRainlogFilters)
+  .all(methodNotAllowed(['GET']));
 
 export default router;
--- src/validations/rainlog.validations.ts
+++ src/validations/rainlog.validations.ts
@@ -45,3 +45,50 @@ export const getRainlogByIdValidation: ValidationChain[] = [
     .isMongoId()
     .withMessage('ID must be a valid MongoDB ObjectId')
 ];
+
+export const getRainlogFiltersValidation: ValidationChain[] = [
+  query('date')
+    .optional()
+    .isISO8601()
+    .withMessage('date must be in ISO 8601 format (e.g. 2026-02-04)')
+    .bail()
+    .toDate()
+    .custom((_value, { req }) => {
+      if (req.query?.dateFrom || req.query?.dateTo) {
+        throw new Error('date cannot be combined with dateFrom or dateTo');
+      }
+      return true;
+    }),
+  query('dateFrom')
+    .optional()
+    .isISO8601()
+    .withMessage('dateFrom must be in ISO 8601 format (e.g. 2026-02-04)')
+    .bail()
+    .toDate(),
+  query('dateTo')
+    .optional()
+    .isISO8601()
+    .withMessage('dateTo must be in ISO 8601 format (e.g. 2026-02-04)')
+    .bail()
+    .toDate(),
+  query('realReading')
+    .optional()
+    .isBoolean()
+    .withMessage('realReading must be a boolean')
+    .bail()
+    .toBoolean(),
+  query('location')
+    .optional()
+    .isString()
+    .withMessage('location must be a string')
+    .bail()
+    .isLength({ min: 1 })
+    .withMessage('location cannot be empty'),
+  query('loggedBy')
+    .optional()
+    .isString()
+    .withMessage('loggedBy must be a string')
+    .bail()
+    .isLength({ min: 1 })
+    .withMessage('loggedBy cannot be empty')
+];
