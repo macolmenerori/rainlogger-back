cc3eeed15bdb5655387cc97e8d83b337af23e22d
macolmenerori <miguelangelcolmenero@gmail.com>
2026-02-05 10:56:14 +0100

    feat: Add security measurements to API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,47 @@
 import compression from 'compression';
+import cors from 'cors';
 import express, { Request, Response } from 'express';
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
 
 import rainloggerRouter from './routes/rainloggerRouter';
 
 const app = express();
+
+const cors_whitelist = process.env.CORS_WHITELIST ? process.env.CORS_WHITELIST.split(',') : [];
+const corsOptions = {
+  origin: function (
+    origin: string | undefined,
+
+    callback: (err: Error | null, allow?: boolean) => void
+  ) {
+    // Check if the origin is in the whitelist
+    if (cors_whitelist.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true // Allow credentials (cookies) to be sent
+};
+
+app.use(cors(corsOptions));
+
+// Handle preflight requests for complex requests (e.g., with credentials)
+app.options('/*splat', cors(corsOptions));
+
+// Add security HTTP headers
+app.use(helmet({ contentSecurityPolicy: false }));
+
+// Rate limit. Default is 100 requests per hour
+const limiter = rateLimit({
+  max: parseInt(
+    process.env.RATELIMIT_MAXCONNECTIONS ? process.env.RATELIMIT_MAXCONNECTIONS : '100'
+  ),
+  windowMs: parseInt(process.env.RATELIMIT_WINDOWMS ? process.env.RATELIMIT_WINDOWMS : '3600000'),
+  message: 'Too many requests from this IP, please try again after an hour'
+});
+app.use('/api', limiter);
 
 // Middleware, modifies incoming data. For parsing JSON bodies on POST requests
 app.use(express.json({ limit: '1024kb' })); // Do not accept bodies bigger than 1 megabyte
